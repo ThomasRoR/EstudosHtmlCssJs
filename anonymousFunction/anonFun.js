127c3d9aad6dc81bdb44e4aa8e81688885659ec4
Thomas Oliveira <103011373+ThomasRoR@users.noreply.github.com>
2023-07-25 13:53:31 -0300

    feat: add anonymou function study

---
+++ anonymousFunction/anonFun.js
@@ -0,0 +1,7 @@
+let vel2 = function(vel){
+    return vel*2
+}
+
+let newVel = vel2(20)
+
+console.log(newVel);
---
+++ anonymousFunction/anonFun2.js
@@ -0,0 +1,9 @@
+console.log(ace(60,10));
+console.log(vel2(50));
+
+let vel2 = function (vel) {
+    return vel * 2
+}
+function ace(vel, acel) {
+    return vel + acel
+}
